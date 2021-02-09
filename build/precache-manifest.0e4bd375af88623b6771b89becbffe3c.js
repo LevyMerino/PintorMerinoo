a6712d6445df52f42e85a1bbb900454b27d79e07
LM <merino.levy@gmail.com>
2021-02-08 22:21:53 -0600

    deploy

--- build/precache-manifest.0e4bd375af88623b6771b89becbffe3c.js
+++ build/precache-manifest.0e4bd375af88623b6771b89becbffe3c.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1a66ed9fe342dc073fbeda3c39c1ad4e",
+    "revision": "3325b852ee30cc0967841b48f8c987e6",
     "url": "/index.html"
   },
   {
-    "revision": "c4ea7d30c40b1275e7c6",
-    "url": "/static/css/main.e447d056.chunk.css"
+    "revision": "92d9077d690a4c96e772",
+    "url": "/static/css/main.8b4d56c5.chunk.css"
   },
   {
     "revision": "b57bdf5e407f0ffb90a4",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.9700d8bd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c4ea7d30c40b1275e7c6",
-    "url": "/static/js/main.be45dcf9.chunk.js"
+    "revision": "92d9077d690a4c96e772",
+    "url": "/static/js/main.47370756.chunk.js"
   },
   {
     "revision": "6fc4f2c3fd5ae8d05fd1",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63e3bfd665c268908ff1fac7232ebf53.js"
+  "/precache-manifest.0e4bd375af88623b6771b89becbffe3c.js"
 );
 
 self.addEventListener('message', (event) => {
